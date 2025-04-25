6f214a32a94d65997eb09d9c98bc5f67661f196d
TnkSinatra <bw.watts007@gmail.com>
2025-04-25 12:35:57 +0100

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -23,3 +23,17 @@ function App() {
 }
 
 export default App;
+*/
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
